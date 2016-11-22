eb3be18017b6d2b51b3714378f470081847cd11e
thiago.procaci <thiago.procaci>
2016-11-22 15:05:48 -0200

    new component - Line chart

--- front-end/src/js/components/VmstatCharts.jsx
+++ front-end/src/js/components/VmstatCharts.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { connect } from "react-redux"
-import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
 
 import MachineSelector from './MachineSelector.jsx';
+import MemoryLineChart from './chart/MemoryLineChart.jsx';
+
+
 import { getAuditList } from "../actions/auditActions";
 import { getMachineList } from "../actions/machineActions";
 
@@ -35,20 +37,8 @@ class VmstatCharts extends React.Component {
 			  		<MachineSelector machineList={machineList} onSelectMachine={this.selectMachine.bind(this)} />
 			  	</div>
 			  	<div>
-					<LineChart width={800} height={600} data={auditList}>
-				       <XAxis dataKey="date" padding={{left: 30, right: 30}}/>
-				       <YAxis/>
-				       <CartesianGrid strokeDasharray="3 3"/>
-				       <Tooltip/>
-				       <Legend />
-				       <Line type="monotone" dataKey="totalMemory" name="Total Memory" stroke="#8884d8" activeDot={{r: 8}}/>
-				       <Line type="monotone" dataKey="usedMemory" name="Used Memory" stroke="#82ca9d" activeDot={{r: 7}} />
-				       <Line type="monotone" dataKey="activeMemory" name="Active Memory" stroke="#ff0000" activeDot={{r: 6}} />
-				       <Line type="monotone" dataKey="inactiveMemory" name="Inactive Memory" stroke="#000099" activeDot={{r: 5}} />       
-				       <Line type="monotone" dataKey="freeMemory" name="Free Memory" stroke="#00cc00" activeDot={{r: 3}} />
-				       <Line type="monotone" dataKey="bufferMemory" name="Buffer Memory" stroke="#333300" activeDot={{r: 2}} />
-				  </LineChart>
-			    </div>			    
+					<MemoryLineChart auditList={auditList}></MemoryLineChart>
+			    </div>			    			    			    
 			  </div> 
 		    )
 	
---
+++ front-end/src/js/components/chart/MemoryLineChart.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
+class MemoryLineChart extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {      
+    let html = (<div></div>);       
+    if(this.props.auditList && this.props.auditList.length > 0) {
+      html = (
+        <div>
+          Memory Chart (GB)
+          <LineChart width={800} height={600} data={this.props.auditList}>
+                 <XAxis dataKey="date" padding={{left: 30, right: 30}}/>
+                 <YAxis/>
+                 <CartesianGrid strokeDasharray="3 3"/>
+                 <Tooltip/>
+                 <Legend />
+                 <Line type="monotone" dataKey="totalMemory" name="Total Memory" stroke="#8884d8" activeDot={{r: 8}}/>
+                 <Line type="monotone" dataKey="usedMemory" name="Used Memory" stroke="#82ca9d" activeDot={{r: 7}} />
+                 <Line type="monotone" dataKey="activeMemory" name="Active Memory" stroke="#ff0000" activeDot={{r: 6}} />
+                 <Line type="monotone" dataKey="inactiveMemory" name="Inactive Memory" stroke="#000099" activeDot={{r: 5}} />       
+                 <Line type="monotone" dataKey="freeMemory" name="Free Memory" stroke="#00cc00" activeDot={{r: 3}} />
+                 <Line type="monotone" dataKey="bufferMemory" name="Buffer Memory" stroke="#333300" activeDot={{r: 2}} />
+            </LineChart>
+        </div>
+        )
+    }           
+    return (html);
+  }
+}
+
+export default MemoryLineChart;
